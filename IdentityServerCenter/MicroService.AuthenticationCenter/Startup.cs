cea95adf576b46dbf64d6dc68a4132721ed9db7e
SenYous <1358798471@qq.com>
2020-11-26 10:33:05 +0800

    2020-11-27

---
+++ IdentityServerCenter/MicroService.AuthenticationCenter/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroService.AuthenticationCenter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()    //默认的开发者证书
+                .AddInMemoryClients(InitConfig.GetClients())
+                .AddInMemoryApiResources(InitConfig.GetApiResources())
+                .AddInMemoryApiScopes(InitConfig.GetApiScopes());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();//添加认证中间件
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ IdentityServerCenter/Microservice.ServoceInstanceAuthen/Controllers/TestController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.ServoceInstanceAuthen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        #region private
+        private readonly ILogger<TestController> _logger;
+        private IConfiguration _iConfiguration;
+        public TestController(ILogger<TestController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this._iConfiguration = configuration;
+        }
+
+        #endregion
+
+
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            Console.WriteLine($"This is TestController {this._iConfiguration["port"]}|Invoke Index");
+            return new JsonResult(new
+            {
+                message = "This is TestController Index",
+                Port = this._iConfiguration["port"],
+                Time = DateTime.Now.ToString("G")
+            });
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("Exception")]
+        public IActionResult IndexA()
+        {
+            Console.WriteLine($"This is TestController {this._iConfiguration["port"]}|Invoke IndexA");
+            return new JsonResult(new
+            {
+                message = "This is TestController IndexA",
+                Port = this._iConfiguration["port"],
+                Time = DateTime.Now.ToString("G")
+            });
+        }
+    }
+}
